ac2cd7baf16af127222a73be46351194b30c3258
haisamk18 <khurshidhaisam@gmail.com>
2023-08-01 00:10:47 +0530

    Home

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './About.scss'
+
+import MongoDb from '../mongodb.png'
+import Express from '../express.png'
+import ReactImg from '../react.png'
+import Node from '../node.png'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='aboutcon'>
+      <div className="abouthead">ABOUT</div>
+      <div className="aboutwrapper">
+      <div className="firstsec"> I'm Haisam, a curious and passionate Full-Stack Developer hailing from Aligarh, India. Embracing both creativity and logic, I find my home in the world of coding, where each line becomes an opportunity to build something incredible. My journey in software development has been an exhilarating ride through diverse technologies - from bringing captivating front-end designs to life using React and Vue.js, to crafting robust back-end systems with Node.js and Python. I'm on a mission to make a difference through ethical and accessible coding, creating applications that not only solve problems but also leave a positive impact on people's lives. Innovation is my compass, and I'm always eager to explore fresh challenges that push the boundaries of what's possible. </div>
+      <div className="techstack">
+        <span>My Tech Stack :-</span><img src={MongoDb}></img><img src={Express}></img><img src={ReactImg}></img><img src={Node}></img>  
+      </div>
+      </div> 
+    </div>
   )
 }
 
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './Home.scss'
+import {useNavigate} from 'react-router-dom';
+import Img from '../haisamimg.png'
+import {AiOutlineDownCircle} from 'react-icons/ai'
+
 
 function Home() {
+  const navigate=useNavigate();
   return (
-    <div>Home</div>
+    <div className='homecon'>
+      <div className="leftcon">
+        <div className='textall'>
+        <div className="greetings">Hello, I'm</div>
+        <div className="name">Haisam Khurshid</div>
+        <div className="designation">Full Stack Developer</div>
+        <div className="breif">a dedicated full-stack developer on a mission to transform creative ideas into functional realities. With a keen eye for detail and a passion for elegant code, I specialize in delivering top-notch web applications that leave a lasting impression.</div>
+        </div>
+      </div>
+      <div className="rightcon">
+        <img src={Img}></img>
+      </div>
+      <AiOutlineDownCircle onClick={()=> navigate('/about')} className='dropdown'/>
+    </div>
   )
 }
 
