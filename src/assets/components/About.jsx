e1aa1d2b0a21416f7e19d0cce8359b45aa79edc6
HAISAM KHURSHID <93199114+haisamk18@users.noreply.github.com>
2025-12-02 18:50:37 +0530

    Update About.jsx

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -15,9 +15,13 @@ function About() {
     <div className='aboutcon'>
       <div className="abouthead">ABOUT</div>
       <div className="aboutwrapper">
-      <div className="firstsec">  I am
-        <div>I’m Haisam, a Full-Stack Developer & Mechanical Engineer passionate about building technology that connects software with EV systems. I develop modern web apps using the MERN stack and also work on motor testing, data acquisition, and EV performance tools.</div>
-        <div>Curiosity and problem-solving define my work—I love creating solutions that are clean, meaningful, and accessible to everyone.</div>
+      <div className="firstsec">
+        <div>
+          I’m Haisam, a Full-Stack Developer & Mechanical Engineer passionate about building technology that connects software with EV systems. I develop modern web apps using the MERN stack and also work on motor testing, data acquisition, and EV performance tools.
+        </div>
+        <div>
+          Curiosity and problem-solving define my work—I love creating solutions that are clean, meaningful, and accessible to everyone.
+        </div>
        
        </div>
       <div className="techstack">
